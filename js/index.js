0f045e865d0932eb64610056ece469ff2ff8c15c
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-05-10 22:31:21 +0200

    Add all page sections in desktop resolution

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$('#Vector_Smart_Object').click(function(){
+    $(this).css('transform', 'rotate(360deg)');
+    $(this).css('-webkit-transform', 'rotate(360deg)');
+    $(this).css('-moz-transform', 'rotate(360deg)');
+});
