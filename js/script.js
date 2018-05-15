08336fd96033c3136c2939bf69e32a6f1a8ab28e
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-05-15 22:15:28 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*var burger = document.getElementById('js-burger-nav');
+var close = document.getElementById('js-close-nav');
+
+burger.addEventListener("click", function() { 
+	close.style.display = "block";
+});
+
+/*close.addEventListener("click", function() { 
+	burger.style.display = "none";
+});*/
+
 $(function() {
 	var i = 1;
 	$('#svg-logo').on("click", function() {
@@ -7,13 +18,13 @@ $(function() {
 
 	$('#js-burger-nav, #js-close-nav').on("click", function() {
 		$('.top-wrapper, .js-dropdown').toggleClass("open");
-		//$('#js-burger-nav, #js-close-nav').toggle();
 	});
 
 	$('#js-burger-nav').on("click", function() {
 		$('.mobile-socials').css({"display":"inline-block", "margin":"26px 0 0 0", "padding-left":"21px"});
 	});
 	
+	// ikonki 
 	$('.burger-wrapper').click( function() {
        $('.close-wrapper').toggle();
     });
