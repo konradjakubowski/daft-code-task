6d2179741a3dd99a09b25e75ca462386272e38de
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-05-15 17:49:42 +0200

    Add small dropdown menu fixes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ $(function() {
 		$(this).css('transform', 'rotate('+ (i * 360) + 'deg)');
 		i++;
 	});
+
 	$('#js-burger-nav').on("click", function() {
 		$('.top-wrapper, .js-dropdown').toggleClass("open");
 	});
+
+	$('#js-close-nav').on("click", function() {
+		$('.top-wrapper, .js-dropdown').toggleClass("open");
+	});
+	/*
+	$("#js-close-nav").click(function () {
+    	$(".burger-wrapper").hide();
+	});*/
+
 });
